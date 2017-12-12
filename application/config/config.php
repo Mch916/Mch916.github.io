9a8b3e709ca63dd78f12a8cd8e9dbbc910b6b2dd
Mch916 <chunho1992@gmail.com>
2017-12-12 18:22:25 +0800

    test

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://118.141.24.200/partyroom/kh/CodeIgniter/';
+$config['base_url'] = 'http://mch916.github.io';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://118.141.24.200/partyroom/kh/CodeIgniter/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
